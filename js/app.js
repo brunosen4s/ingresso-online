0aba3658171b0aece76168a2d13a807d911edf12
Bruno Sena <brunosen4s@gmail.com>
2023-11-08 15:31:05 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function comprar(){
+    let ingresso = document.getElementById('tipo-ingresso').textContent;
+    let quantidade = document.getElementById('qtd').value;
+   if (ingresso == 'pista'){
+    comprarPista(qtd.value);
+
+   }
+}
+
+function comprarPista(){
+    
+}
